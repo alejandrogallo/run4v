c5683cf74a80edd51241fe15e13548b7a12f9f91
Alejandro Gallo <gallo@pccq05.fkf.mpg.de>
2015-11-25 11:31:24 +0100

    Fixed INCAR test

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 clean: 
 	rm ./**/*.pyc
 	rm *.pyc
+
+test: 
+	python -m unittest discover -v tests
--- tests/test_vaspfiles.py
+++ tests/test_vaspfiles.py
@@ -1,7 +1,7 @@
 from common import * 
 import unittest 
 
-
+run4v.VERBATIM = False
 
 class TestVaspFile(unittest.TestCase):
     def setUp(self):
@@ -18,14 +18,12 @@ class TestVaspFile(unittest.TestCase):
 
 class TestIncar(TestVaspFile):
     def setUp(self):
-        self.settings = {'IBRION':2, 'ISPIN':'2','ENCUT' : '400'}#,'NELECT':'046','NELM':'50','ISMEAR':'-2','FERWE':'256*1 128*0','FERDO':'254*1 130*0','NBANDS':'384'}
+        self.settings = {'ISPIN':'2', 'IBRION':2, 'ENCUT' : '400'}
         self.fileName = 'TEST_INCAR'
         self.vaspFile = run4v.INCAR(self.settings, fileName = self.fileName)
         self.vaspFile.createFile()
     def test_getContents(self):
         contents = self.vaspFile.getContents()
-        expectedContents = "IBRION=2\nISPIN=2\nENCUT=400"
+        expectedContents = "ISPIN=2\nIBRION=2\nENCUT=400"
         self.assertTrue(expectedContents in contents)
 
-if __name__=="__main__":
-    unittest.main()
